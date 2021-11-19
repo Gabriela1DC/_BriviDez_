42051b7b22e79fd7da79b6cf4fd62221c4f1c3f9
Gabriela Costa <davidbia6@gmail.com>
2021-11-19 16:51:10 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import {NgbPaginationModule, NgbAlertModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormularioComponent } from './contato/formulario/formulario.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    NgbPaginationModule,
+    NgbAlertModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contato/shared/contato.ts
@@ -0,0 +1,8 @@
+export class Contato {
+    name:string='';
+    email:string='';
+    telefone:string='';
+    assunto:string='';
+    mensagem:string='';
+    termos:boolean=false;
+}
