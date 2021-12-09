361ff8441cf404643737b0c2afbe4684dd58a539
Gabriela Costa <davidbia6@gmail.com>
2021-12-08 23:32:31 -0300

    Correção na declaração do formGroup

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -7,12 +7,30 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./formulario.component.scss']
 })
 export class FormularioComponent implements OnInit {
+  formContact!: FormGroup; 
 
-  constructor(
-    private formBuilder: FormBuilder
-  ) { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  get f() {
+    return this.formContact.controls
+  }
 
   ngOnInit(): void {
+
+    
+    this.formContact=this.formBuilder.group({
+      name:[null, [Validators.required, Validators.minLength(3)]],
+      email:[null, [Validators.required, Validators.email]],
+      tel:[null, [Validators.required, Validators.minLength(11),Validators.maxLength(11)]],
+      assunto:[null,Validators.required],
+      mensagem:[null,[Validators.required,Validators.minLength(150)]],
+      check:['',Validators.requiredTrue],
+    });
+
+  }
+
+  onSubmit() {
+    console.log(this.formContact.value)
   }
 
 }
