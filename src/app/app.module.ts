fbcaa69001f67e221b108859d868f1656949915e
Gabriela Costa <davidbia6@gmail.com>
2021-12-09 21:01:02 -0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { FormularioComponent } from './formulario/formulario.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { FormularioModule } from './formulario/formulario.module';
+import { PhonePipe } from './shared/phone-pipe/phone.pipe';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { FormularioModule } from './formulario/formulario.module';
     HeaderComponent,
     FooterComponent,
     FormularioComponent,
+    PhonePipe,
   ],
   imports: [
     BrowserModule,
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -8,6 +8,8 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 })
 export class FormularioComponent implements OnInit {
   formContact!: FormGroup; 
+
+   @ViewChild('fform') formularioFormDirective:any;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -21,16 +23,84 @@ export class FormularioComponent implements OnInit {
     this.formContact=this.formBuilder.group({
       name:[null, [Validators.required, Validators.minLength(3)]],
       email:[null, [Validators.required, Validators.email]],
-      tel:[null, [Validators.required, Validators.minLength(11),Validators.maxLength(11)]],
+      tel:[null, [Validators.required, Validators.minLength(11),Validators.maxLength(12)]],
       assunto:[null,Validators.required],
-      mensagem:[null,[Validators.required,Validators.minLength(150)]],
+      mensagem:[null,[Validators.required,Validators.minLength(10)]],
       check:['',Validators.requiredTrue],
     });
 
+    
   }
 
   onSubmit() {
+     
     console.log(this.formContact.value)
+    console.log(this.formContact.controls)
+    console.log(this.formContact.valid)/*
+
+    name=this.formContact.value.name
+    invalidErro=() =>{
+      if(name.invalid || name.untouched || name.dirty){
+        document.write
+      }
+    }
+    
+
+    if (this.formContact.touched){
+      if (this.formContact.valid==false){
+        alert('Formulário Inválido')
+        this.formContact.reset()
+      }else{
+      alert('Formulário enviado com sucesso')
+      this.formContact.reset()
+    }
+    }*/
+
+    const validateName = () => {
+        const name = this.formContact.value.name;
+        if(name.value.length < 3) {
+            name.nextElementSibling.classList.add('error');
+        } else {
+            name.nextElementSibling.classList.remove('error');
+        }
+    }
+
+    const validateEmail = () => {
+        const email = this.formContact.value.email;
+        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const emailTest = regex.test(email.value);
+
+        if(!emailTest) {
+            email.nextElementSibling.classList.add('error');
+        } else {
+            email.nextElementSibling.classList.remove('error');
+        }
+    }
+
+    const validateTel = () => {
+        const tel = this.formContact.value.tel;
+        var regexT = new RegExp('^\\(((1[1-9])|([2-9][0-9]))\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$'); 
+        const telTest = regexT.test(tel.value);
+
+        if(tel.value.length < 11 && !telTest) {
+            tel.nextElementSibling.classList.add('error');
+        } else {
+            tel.nextElementSibling.classList.remove('error');
+        }
+    }
+
+    if (this.formContact.touched){
+      if (this.formContact.valid==false){
+        alert('Formulário Inválido')
+        this.formContact.reset()
+      }else{
+      alert('Formulário enviado com sucesso')
+      this.formContact.reset()}
+    }else{
+      alert('O Formulário Inválido: O formulárido não está preenchido corretamente !!!')
+    }
+
+
   }
 
 }
